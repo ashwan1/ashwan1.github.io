8de2c7e342d7600197d7f124cd7bf308f561bba1
Ashwani Pandey <ashwani472bmt@gmail.com>
2022-08-18 00:36:32 +0530

    Complete use case outline

--- posts/ai4plasticpollution/components/bind-event-to-svg.js
+++ posts/ai4plasticpollution/components/bind-event-to-svg.js
@@ -8,17 +8,22 @@ class BindEventToSvg extends React.PureComponent {
         document.querySelectorAll("#mlUses svg text").forEach(item => {
             item.addEventListener('click', e => {
                 var targetElement = e.currentTarget;
+                if (targetElement.innerHTML.toLowerCase().includes('ml for')) {
+                    return;
+                }
+                document.querySelectorAll("#mlUses svg text").forEach(t => {t.style.fill = "black"});
+                targetElement.style.fill = 'gray';
                 if (targetElement.innerHTML.toLowerCase().includes('packaging')) {
-                    document.getElementById('mlUsesDesc').innerHTML = 'You clicked efficient packaging.';
+                    document.getElementById('mlUsesDesc').innerHTML = document.getElementById('packaging').innerHTML;
                 }
                 if (targetElement.innerHTML.toLowerCase().includes('tracking')) {
-                    document.getElementById('mlUsesDesc').innerHTML = 'You clicked tracking plastics.';
+                    document.getElementById('mlUsesDesc').innerHTML = document.getElementById('tracking').innerHTML;
                 }
                 if (targetElement.innerHTML.toLowerCase().includes('separating')) {
-                    document.getElementById('mlUsesDesc').innerHTML = 'You clicked separating plastics.';
+                    document.getElementById('mlUsesDesc').innerHTML = document.getElementById('separating').innerHTML;
                 }
                 if (targetElement.innerHTML.toLowerCase().includes('decomposition')) {
-                    document.getElementById('mlUsesDesc').innerHTML = 'You clicked plastic decomposition.';
+                    document.getElementById('mlUsesDesc').innerHTML = document.getElementById('decomposition').innerHTML;
                 }
             })
         });
